66bec2a67855c11d72deea624969769939ea0d3f
Pablo <jpvar04@gmail.com>
2016-11-10 09:01:10 -0600

    creadas las tablas de facturas y productos facturas para las compras

---
+++ config/Migrations/20161109152355_create_facturas.php
@@ -0,0 +1,54 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateFacturas extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('facturas',  ['id' => false, 'primary_key' => ['idFactura']]);
+        $table->addColumn('idFactura', 'string', [
+            'default' => null,
+            'limit' => 50,
+            'null' => false,
+        ]);
+        $table->addColumn('fechaFactura', 'date', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('idUsuario', 'string', [
+            'default' => null,
+            'limit' => 50,
+            'null' => false,
+        ]);
+        $table->addForeignKey('idUsuario', 'personas', 
+            'identificacion', array('delete' => 'NO_ACTION', 'update' => 'CASCADE')
+        );
+        
+        /*$table->addForeignKey('idUsuario', 'tarjetas', 'idPersona',
+         array('delete' => 'NO_ACTION', 'update' => 'CASCADE')
+        );*/
+        $table->create();
+    }
+}
---
+++ config/Migrations/20161109162659_create_productos_facturas.php
@@ -0,0 +1,61 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateProductosFacturas extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+         $table = $this->table('productos_facturas',  ['id' => false, 'primary_key' => ['idFactura', 'idProducto']]);
+        $table->addColumn('idFactura', 'string', [
+            'default' => null,
+            'limit' => 50,
+            'null' => false,
+        ]);
+        $table->addForeignKey('idFactura', 'facturas', 
+            'idFactura',
+            array('delete' => 'NO ACTION', 'update' => 'CASCADE')
+        );
+        $table->addColumn('idProducto', 'string', [
+            'default' => null,
+            'limit' => 50,
+            'null' => false,
+        ]);
+        $table->addForeignKey('idProducto', 'productos', 
+            'idProducto',
+            array('delete' => 'NO ACTION', 'update' => 'CASCADE')
+        );
+        $table->addColumn('precioTotal', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('cantidad', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+         $table->create();
+    }
+}
