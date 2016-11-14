5f09122939c5838631d0157701ef5f3e72efd4e1
felipe <feliperosabal93@gmail.com>
2016-11-14 15:48:38 +0000

    arregla la vista de ofertas

--- src/Controller/ProductosController.php
+++ src/Controller/ProductosController.php
@@ -33,7 +33,7 @@ class ProductosController extends AppController
         $this->set('idProd', $idProd);
         $this->set('user', $this->Auth->user('id'));
         
-        $carrito_compras = $this->Carrito_compras->find('all');
+       // $carrito_compras = $this->Carrito_compras->find('all');
     }
     /**
      * funcion catalogo
@@ -149,18 +149,18 @@ class ProductosController extends AppController
    
     //Controlador de ofertas y combos
     public function ofertas() {
-       /* $numPage = 1;
+        $numPage = 1;
         $nuevaPag = $this -> request -> query('nuevaPag');
         
         if ($nuevaPag && $nuevaPag > 0) {
             $numPage = $nuevaPag;
         }
-        */
+        
         $ofertas = $this -> Productos -> find ('all', 
             ['contain' => ['ofertas']]);
             
-       /* $ofertas = $ofertas -> limit(16) -> page ($numPage);
-        $this -> set ('numPage', $numPage);*/
+        $ofertas = $ofertas -> limit(16) -> page ($numPage);
+        $this -> set ('numPage', $numPage);
         $this->set('ofertas', $ofertas);
             
         $combos = $this -> Productos -> find ('all', 
