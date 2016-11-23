4e00e2f1cd312feacd31092b44fe481f95bf93cf
Oscar Corella <oscorella@gmail.com>
2016-11-23 11:25:03 -0600

    cganges

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -218,9 +218,7 @@ Type::build('datetime')
  * Only try to load DebugKit in development mode
  * Debug Kit should not be installed on a production system
  */
-if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
-}
+
 Plugin::load('Migrations');
 
 Plugin::load('BootstrapUI');
